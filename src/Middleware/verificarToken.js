3ddc249e4f1670c4698e4aa302f744df5a5b79ba
luigy23 <luigyleonardo23@gmail.com>
2023-08-12 10:39:52 -0500

    console log

--- src/Middleware/verificarToken.js
+++ src/Middleware/verificarToken.js
@@ -2,13 +2,19 @@ const jwt = require("jsonwebtoken")
 
 //middleware para verificar el token
 const verificarToken = (req, res, next) => {
-    
+    // permitimos cors
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+
+
+
+
     // sacamos el token de la cabecera Authorization (Bearer <token>)
     const token = req.headers.authorization.split(" ")[1]
-
+    console.log("este es el token: "+token)
     if (!token) {
         res.status(500).json({
-            message: "No hay token"
+            message: "No hay token para verificar"
         })
         return
     }
