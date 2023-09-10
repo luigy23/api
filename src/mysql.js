ed6f6ccc5288453f368de0ed5cd13c22b7dea788
luigy23 <luigyleonardo23@gmail.com>
2023-09-10 17:54:11 -0500

    actualizamos contraseña

--- src/mysql.js
+++ src/mysql.js
@@ -6,7 +6,7 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASS || "",
+  password: process.env.DB_PASS || "123456",
   database: process.env.DB_NAME || "restaurante2",
 });
 
